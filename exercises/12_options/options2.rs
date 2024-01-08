20b7052ba5437606a76e920e3183c5b47869adf5
jerry <jerrycai@shein.com>
2024-01-08 20:12:36 +0800

    rustlings! options

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint options2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
@@ -13,9 +12,10 @@ mod tests {
         let optional_target = Some(target);
 
         // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        if let Some(word) = optional_target { // 如果 optional_target 为 Some(T) 类型，那 word 就是 T
+            assert_eq!(word, target); // 即 word 为 target 的值
         }
+
     }
 
     #[test]
@@ -32,7 +32,7 @@ mod tests {
         // TODO: make this a while let statement - remember that vector.pop also
         // adds another layer of Option<T>. You can stack `Option<T>`s into
         // while let and if let.
-        integer = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -10,10 +10,22 @@ struct Point {
     y: i32,
 }
 
+// fn main() {
+//     let y: Option<Point> = Some(Point { x: 100, y: 200 });
+
+//     match y {
+//     // match &y {
+//         // Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+//         Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+//         _ => panic!("no match!"),
+//     }
+//     y; // Fix without deleting this line.
+// }
+
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
